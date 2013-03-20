8f6ec2e70ecfa73fd7ff40192523b1e64bbcd4d7
Rafal Wrzochol <rafal.wrzochol@fagbokforlaget.no>
2013-03-20 13:09:53 +0100

    fix tests

--- test/textii.test.js
+++ test/textii.test.js
@@ -15,20 +15,20 @@ describe('pdfii', function() {
           throw err;
         }
         var expected = {
-          zero: { page1: [ 0 ], page2: [ 0 ] },
-          three: { page1: [ 3 ], page2: [ 3 ] },
-          five: { page1: [ 5 ], page2: [ 5 ] },
-          six: { page1: [ 6 ], page2: [ 6 ] },
-          seven: { page1: [ 7, 8 ], page2: [ 7, 8 ] },
-          included: { page1: [ 18 ], page2: [ 18 ] },
-          one: { page1: [ 19 ], page2: [ 19 ] },
-          test: { page1: [ 21, 22 ], page2: [ 21, 22 ] },
-          good: { page1: [ 24 ], page2: [ 24 ] },
-          alwai: { page1: [ 25 ], page2: [ 25 ] },
-          now: { page3: [ 0 ] },
-          comma: { page3: [ 2 ] },
-          separ: { page3: [ 3 ] },
-          valu: { page3: [ 4 ] }
+          zero: { '1': [ 0 ], '2': [ 0 ] },
+          three: { '1': [ 3 ], '2': [ 3 ] },
+          five: { '1': [ 5 ], '2': [ 5 ] },
+          six: { '1': [ 6 ], '2': [ 6 ] },
+          seven: { '1': [ 7, 8 ], '2': [ 7, 8 ] },
+          included: { '1': [ 18 ], '2': [ 18 ] },
+          one: { '1': [ 19 ], '2': [ 19 ] },
+          test: { '1': [ 21, 22 ], '2': [ 21, 22 ] },
+          good: { '1': [ 24 ], '2': [ 24 ] },
+          alwai: { '1': [ 25 ], '2': [ 25 ] },
+          now: { '3': [ 0 ] },
+          comma: { '3': [ 2 ] },
+          separ: { '3': [ 3 ] },
+          valu: { '3': [ 4 ] }
         };
         assert(expected.equals(data));
         done();
